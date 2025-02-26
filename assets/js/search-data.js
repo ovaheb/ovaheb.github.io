403f6e1bf4329ae501c56d314883c91d67e6fa45
ovaheb <54933505+ovaheb@users.noreply.github.com>
2025-02-26 03:20:27 +0000

    Deploying to gh-pages from @ ovaheb/ovaheb.github.io@bea3db356a0766e15e1ad4de5a2158b2ec8b92e4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,8 +16,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/repositories/";
           },
-        },{id: "nav-cv",
-          title: "CV",
+        },{id: "nav-curriculum-vitae",
+          title: "Curriculum Vitae",
           description: "",
           section: "Navigation",
           handler: () => {
